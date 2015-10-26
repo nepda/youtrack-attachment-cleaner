ef3eb90f566f8420fb88adc060385b1106b9bfcd
Nepomuk Fraedrich <info@nepda.eu>
2015-10-26 11:19:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# YouTrack attachment cleaner
+
+With this tiny script, you can batch-remove attachments from issues by query.
+
+## Parameters
+
+* `h|host` The youtrack host/URL
+* `u|username` The login username
+* `p|password` The login password
+* `q|query` The search query for the issues
+* `f|pattern` An optional filter/pattern for attachment filenames
+* `l|limit` The limit/maximum of issues fetched, default is 100.
+
+## Example
+
+To delete the first 1000 matching resolved issues in project 'Test' with attachments, which are
+created between 1990-01-01 and 2015-01-01.
+
+    php src/cleaner.php \
+        -h https://**.myjetbrains.com/youtrack \
+        -u root -p '**secret**' \
+        -q 'project: Test has: attachments created: 1990-01-01 .. 2015-01-01 State: Resolved' \
+        --pattern 'backtrace|logfile' \
+        --limit 1000
+
+You can run this line as long as no results are found.
---
+++ src/cleaner.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Ulrichsg\Getopt\Getopt;
+use Ulrichsg\Getopt\Option;
+
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+$getopt = new Getopt(
+    [
+        new Option('h', 'host', Getopt::REQUIRED_ARGUMENT),
+        new Option('u', 'username', Getopt::REQUIRED_ARGUMENT),
+        new Option('p', 'password', Getopt::REQUIRED_ARGUMENT),
+        new Option('q', 'query', Getopt::REQUIRED_ARGUMENT),
+        new Option('f', 'pattern', Getopt::OPTIONAL_ARGUMENT),
+        new Option('l', 'limit', Getopt::OPTIONAL_ARGUMENT),
+    ]
+);
+
+try {
+    $getopt->parse();
+
+    $query = $getopt['query'];
+    $limit = $getopt['limit'] ? $getopt['limit'] : 100;
+
+    $youtrack = new YouTrack\Connection(
+        $getopt['host'],
+        $getopt['username'],
+        $getopt['password']
+    );
+
+    $pattern = $getopt['pattern'];
+
+    echo 'Executing query: "' . $query . '"' . PHP_EOL;
+
+    $issues = $youtrack->getIssuesByFilter($query, null, $limit);
+
+    echo count($issues) . ' issues found' . PHP_EOL;
+
+    foreach ($issues as $issue) {
+
+        $attachments = $issue->getAttachments();
+        $ac = count($attachments);
+        $ts = substr($issue->created, 0, -3);
+        $created = new \DateTime('@' . $ts);
+        echo $issue->getId() . ', created: ' . $created->format('Y-m-d H:i:s') . ': ' . $ac . ' attachments.' . PHP_EOL;
+        foreach ($attachments as $attachment) {
+            echo '    ID: ' . $attachment->getId() . ' (' . $attachment->getName() . ')';
+            if (!$pattern || preg_match('/' . $pattern . '/', $attachment->getName())) {
+                $success = $youtrack->deleteAttachment($issue, $attachment);
+                if ($success) {
+                    echo ' deleted';
+                } else {
+                    echo ' NOT deleted';
+                }
+                echo PHP_EOL;
+            } else {
+                echo ' skipped';
+            }
+        }
+        echo PHP_EOL;
+    }
+    exit(0);
+
+} catch (UnexpectedValueException $e) {
+    echo "Error: " . $e->getMessage() . PHP_EOL;
+    echo $getopt->getHelpText();
+    exit(1);
+}
